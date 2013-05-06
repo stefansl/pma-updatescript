c88bd9985bbb0d1a3010255d1c9f3ecc7f58746b
Michael Riehemann <pitgrap@gmx.net>
2013-05-06 16:39:55 +0200

    add different output modes (verbose and quiet)

--- pma-update.sh
+++ pma-update.sh
@@ -11,17 +11,30 @@ PMA=""          #name of the phpmyadmin folder. f.e. pma or phpMyAdmin
 USER=""         #User
 GROUP=""        #Group
 VERSIONLINK="http://www.phpmyadmin.net/home_page/version.php"
-
-
+QUIET=0         #set 1 if you don't want any output at all
+VERBOSE=0       #set 1 to output all messages (overrides QUIET)
 
 ##
 # Don't change anything from here
 ##
 
+# output warnings
+function log() {
+    if [ $QUIET -ne 1 -o $VERBOSE -eq 1 ]; then
+        echo "$@";
+    fi
+}
+
+# output additional messages
+function info() {
+    if [ $VERBOSE -eq 1 ]; then
+        echo "$@";
+    fi
+}
 
 # Check settings
-if [  -z "$LOCATION" -o -z "$PMA" -o -z "$USER" -o -z "$GROUP" ]; then
-    echo "Check your settings, please. LOCATION, PMA, USER and/or GROUP variables are still empty!";
+if [ -z "$LOCATION" -o -z "$PMA" -o -z "$USER" -o -z "$GROUP" ]; then
+    log "Check your settings, please. LOCATION, PMA, USER and/or GROUP variables are still empty!";
     exit 1;
 fi
 
@@ -29,12 +42,11 @@ fi
 if [ -f $LOCATION/$PMA/README ];
 then
     VERSIONLOCAL=$(sed -n 's/^Version \(.*\)$/\1/p' $LOCATION/$PMA/README);
-    echo "Found local installation version" $VERSIONLOCAL;
+    info "Found local installation version" $VERSIONLOCAL;
 else
-    echo "Did not found a working installation. Check your settings, please.";
+    log "Did not found a working installation. Check your settings, please.";
     exit 1;
 fi
-
 
 # Get latest version
 if [ -n "$1" ]; then
@@ -43,7 +55,7 @@ if [ -n "$1" ]; then
 
     #Check the versions
     if [ $VERSION = $VERSIONLOCAL ]; then
-        echo "phpMyAdmin" $VERSIONLOCAL "is installed already!";
+        info "phpMyAdmin" $VERSIONLOCAL "is installed already!";
         exit 0;
     fi
 else
@@ -52,7 +64,7 @@ else
 
     #Check the versions
     if [ $VERSION = $VERSIONLOCAL ]; then
-        echo "Your phpMyAdmin installation is already the newest!";
+        info "Your phpMyAdmin installation is already the newest!";
         exit 0;
     fi
 fi
@@ -66,23 +78,23 @@ if [ -n "$VERSION" ]; then
 
     if [ $MYLOCATION != $LOCATION ]
     then
-        echo "An error occured while changing the directory. Please check your settings! Your given directory:" $LOCATION;
+        log "An error occured while changing the directory. Please check your settings! Your given directory:" $LOCATION;
         pwd;
 
     else
         wget --directory-prefix=$LOCATION http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/$VERSION/phpMyAdmin-$VERSION-all-languages.tar.bz2
         if [ -f "$LOCATION/phpMyAdmin-$VERSION-all-languages.tar.bz2" ]
-        then    tar xjvf phpMyAdmin-$VERSION-all-languages.tar.bz2
+        then
+            tar xjvf phpMyAdmin-$VERSION-all-languages.tar.bz2
             mv -v $LOCATION/$PMA/config.inc.php $LOCATION/phpMyAdmin-$VERSION-all-languages/
             rm -Rv $LOCATION/$PMA
             mv -v $LOCATION/phpMyAdmin-$VERSION-all-languages $LOCATION/$PMA
             chown -Rv $USER:$GROUP $LOCATION/$PMA
-            echo "I succesfully updated phpMyAdmin from version " $VERSIONLOCAL " to " $VERSION " in your directory " $LOCATION ". Enjoy!"
+            log "I succesfully updated phpMyAdmin from version " $VERSIONLOCAL " to " $VERSION " in your directory " $LOCATION ". Enjoy!"
         else
-            echo "An error occured while downloading. I tried downloading from: http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/"$VERSION"/phpMyAdmin-"$VERSION"-all-languages.tar.bz2.";
+            log "An error occured while downloading. I tried downloading from: http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/"$VERSION"/phpMyAdmin-"$VERSION"-all-languages.tar.bz2.";
         fi
     fi
 else
-    echo "Something went wrong while detecting the newest Version of PMA. :( Maybe this link here is dead: $VERSIONLINK";
-
+    log "Something went wrong while detecting the newest Version of PMA. :( Maybe this link here is dead: $VERSIONLINK";
 fi
